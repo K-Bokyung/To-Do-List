5ae4c7e885dc4bdfcee4f774dada35ba279fee73
Bokyung Kwon <tree445560@gmail.com>
2024-09-05 14:26:29 +0900

    add: 할 일 생성, 할 일 삭제, 목록 없을 때 체크박스와 삭제 버튼 클릭 못하는 기능 추가

--- src/main.js
+++ src/main.js
@@ -1,49 +1,42 @@
 // 1. to-do-list 버튼 기능
 let buttons = document.querySelectorAll('button');
+
 buttons.forEach((button) => {
-  if (button.className === 'submit__create') {
+  if (button.className === 'submit__btn') {
     button.addEventListener('click', createList);
-  } else if (button.className === 'lists__delete') {
-    button.addEventListener('click', deleteList);
+  } else if (button.className === 'delete__btn') {
+    button.addEventListener('click', function (event) {
+      event.preventDefault();
+      deleteList(button.value);
+    });
   }
 });
 
-// 2. todolist array 생성 (최대 5개)
-let todoList = [];
-const listLength = todoList.length;
+// 2. 할 일 생성 (최대 5개)
+const todoList = [];
+let inputs = document.getElementsByClassName('lists__todo');
+let checks = document.getElementsByClassName('lists__check');
+let deletes = document.getElementsByClassName('delete__btn');
 
-// 3. create list
 function createList() {
-  if (listLength === 5) {
+  if (todoList.length > 5) {
     alert('최대 5개까지 입력할 수 있어요!');
   } else {
     const input = document.getElementById('write');
     todoList.push(input.value);
+    inputs[todoList.length - 1].value = input.value;
+    checks[todoList.length - 1].disabled = false;
+    deletes[todoList.length - 1].disabled = false;
+    document.getElementById('write').value = '';
   }
-  console.log('create!');
+  // console.log('create!');
 }
 
-// 4. todolist array에 있는 리스트가 목록에 반영
-let inputs = document.querySelectorAll('input');
-inputs.forEach((input) => {
-  if (input.className === 'lists__todo') {
-    input.value = todoList;
-  } else if (input.type === 'checkbox') {
-    // changeCheck();
-    console.log('changeCheck!');
-  }
-});
-
-function writeList(index) {
-  value = todoList[index];
-}
-
-// function changeCheck(index) {
-//   input.value = '';
-//   todoList.pop();
-// }
-
-// 5. delete list
-function deleteList() {
+// 3. 할 일 삭제
+function deleteList(value) {
+  let num = value;
+  inputs[num].value = '';
+  checks[num].disabled = true;
+  deletes[num].disabled = true;
   console.log('delete!');
 }
