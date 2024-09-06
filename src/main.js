68512960c0bc18fbe871c0e5ea6ddff2e808d815
Bokyung Kwon <tree445560@gmail.com>
2024-09-06 13:15:00 +0900

    add: 목록 삭제 후 남은 목록들의 순서가 바뀌는 기능 추가

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ buttons.forEach((button) => {
   } else if (button.className === 'delete__btn') {
     button.addEventListener('click', function (event) {
       event.preventDefault();
-      deleteList(button.value);
+      deleteList(button);
     });
   }
 });
@@ -19,24 +19,33 @@ let checks = document.getElementsByClassName('lists__check');
 let deletes = document.getElementsByClassName('delete__btn');
 
 function createList() {
+  const input = document.getElementById('write');
   if (todoList.length > 5) {
     alert('최대 5개까지 입력할 수 있어요!');
+  } else if (input.value.length === 0) {
+    alert('내용을 입력해주세요!');
   } else {
-    const input = document.getElementById('write');
     todoList.push(input.value);
     inputs[todoList.length - 1].value = input.value;
     checks[todoList.length - 1].disabled = false;
     deletes[todoList.length - 1].disabled = false;
     document.getElementById('write').value = '';
   }
-  // console.log('create!');
 }
 
 // 3. 할 일 삭제
-function deleteList(value) {
-  let num = value;
-  inputs[num].value = '';
-  checks[num].disabled = true;
-  deletes[num].disabled = true;
-  console.log('delete!');
+function deleteList(button) {
+  let num = button.value;
+  todoList.splice(num, 1);
+
+  todoList.forEach((item, index) => {
+    console.log(item, index);
+    inputs[index].value = item;
+    checks[index].checked = checks[index].disabled;
+    deletes[index].disabled = deletes[index].disabled;
+  });
+
+  inputs[todoList.length].value = '';
+  checks[todoList.length].disabled = true;
+  deletes[todoList.length].disabled = true;
 }
